96b3037c03e2305fda5af85ee3b9d69bc7b70002
Raghave Gupta <raghave18@gmail.com>
2022-07-12 19:52:21 +1000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'seed.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+
+csv.each do |row|
+  t = Lock.new
+
+  t.name = row['Name']
+  t.address = row['Address']
+  t.description = row['Description']
+
+  t.save
+end
+
 User.destroy_all
 Lock.destroy_all
 Visit.destroy_all
