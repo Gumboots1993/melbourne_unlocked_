f10446bc96d06a234ade5394f925307741db31f8
Darko Petkovic <darko.petkovic.05@gmail.com>
2022-07-13 01:27:45 +1000

    show page shows correct info, popups updated with show info, map centers to popup, fixed footer covering bottom of body

---
+++ app/javascript/controllers/lock_show_controller.js
@@ -0,0 +1,55 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    console.log("hello");
+        // 💚 success callback, mandatory
+        function success(position) {
+        // target the element containing the location data
+        var elUnlocked = document.querySelector('.unlocked');
+        var elLocked = document.querySelector('.locked');
+        var elLockedClose = document.querySelector('.locked-close');
+        // get the data from the attribute
+        var unlocked = elUnlocked.getAttribute('data-unlocked');
+        var lat = elLocked.getAttribute('data-lat')
+        var lng = elLocked.getAttribute('data-lng')
+        var distance = 0
+        console.log(lng);
+          // set points
+          var from = turf.point([position.coords.latitude, position.coords.longitude]);
+          var to = turf.point([lat, lng]);
+          //set option for turf calc
+          var options = {units: "kilometers"};
+          // turf distance calculation
+          distance = turf.distance(from, to, options);
+
+          console.log(distance);
+
+          if (unlocked === "true") {
+            elLocked.remove();
+            elLockedClose.remove();
+          } else if(distance > 0.6) {
+            elLockedClose.remove();
+            elUnlocked.remove();
+          } else {
+            elLocked.remove();
+            elUnlocked.remove();
+          }
+        }
+        // 🚨 error callback, optional
+        function error (err) {
+          // display error
+          console.log(err);
+        }
+        // options, optional
+        const options = {
+          timeout: 5000,
+          maximumAge: Infinity
+        }
+
+      // 🚀 get user's currnt position
+      navigator.geolocation.getCurrentPosition(success, error, options);
+
+
+  }
+}
