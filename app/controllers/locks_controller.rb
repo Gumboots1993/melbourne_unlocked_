ca21866a32e4c694fd435d70b8a292f312b3cf37
Raghave Gupta <raghave18@gmail.com>
2022-07-23 13:31:16 +1000

    tabs

--- app/controllers/locks_controller.rb
+++ app/controllers/locks_controller.rb
@@ -47,6 +47,7 @@ class LocksController < ApplicationController
 
   def view_requests
     @locks = Lock.where(status: "Pending")
+    @declined_locks = Lock.where(status: "Declined")
   end
 
   def photo
