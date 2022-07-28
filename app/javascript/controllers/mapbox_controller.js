fe52bcb95d93ae77b7fd8fbcdae69d84a1a32c4e
Darko Petkovic <darko.petkovic.05@gmail.com>
2022-07-28 21:37:08 +1000

    added back img urls

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -70,13 +70,13 @@ export default class extends Controller {
           customMarker.className = "marker"
           if (marker.image_url != "") {
             customMarker.setAttribute('data-unlocked', true)
-            customMarker.style.backgroundImage = "url('/assets/raggy.png')";
+            customMarker.style.backgroundImage = "url('assets/unlocked_2.svg')";
           } else if (distance > 0.05) {
             customMarker.setAttribute('data-unlocked', false)
-            customMarker.style.backgroundImage = "url('/assets/raggy.png')";
+            customMarker.style.backgroundImage = "url('assets/locked_1.svg')";
           } else {
             customMarker.setAttribute('data-unlocked', false)
-            customMarker.style.backgroundImage = "url('/assets/raggy.png')";
+            customMarker.style.backgroundImage = "url('assets/unlockable_1.svg')";
           }
           customMarker.setAttribute('data-lat', marker.lat)
           customMarker.setAttribute('data-lng', marker.lng)
