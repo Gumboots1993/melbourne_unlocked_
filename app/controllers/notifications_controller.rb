e4945325413b009893d2029d378e1f8ee191672b
bethany krasey <bethanykrasey@gmail.com>
2022-07-29 19:49:03 +1000

    added a welcome notificaion

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,9 +1,12 @@
 class NotificationsController < ApplicationController
 
   def index
-    @notifications = current_user.notifications
+    @notifications = current_user.notifications.newest_first
     @notifications.each do |notification|
       if notification.type == 'LockSuggestionNotification'
+        notification.mark_as_read!
+      end
+      if notification.type == 'WelcomeNotification'
         notification.mark_as_read!
       end
     end
