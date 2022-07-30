1535912682f58022c5c16fe608b871bf48d675a7
bethany krasey <bethanykrasey@gmail.com>
2022-07-30 16:16:50 +1000

    updated notification styling and added levels column to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :username, :photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :username, :photo, :level])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :username, :photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :username, :photo, :level])
   end
 end
--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -5,6 +5,7 @@ class VisitsController < ApplicationController
     @visit.lock_id = params[:lock_id]
     @visit.unlocked_date = DateTime.now()
     @visit.user = current_user
+
     if @visit.save
       redirect_to visit_path(@visit)
     else
@@ -35,4 +36,34 @@ class VisitsController < ApplicationController
   def visit_params
     params.require(:visit).permit(:photo)
   end
+
+  def levels(visits)
+    case visits
+    when 0..4
+      @user.level = "LVL 0 NOVICE"
+      @user.save
+      "LVL 0 NOVICE"
+    when 5..9
+      @user.level = "LVL 1 LEARNER"
+      @user.save
+      "LVL 1 LEARNER"
+    when 10..19
+      @user.level = "LVL 2 EXPERT"
+      @user.save
+      "LVL 2 EXPERT"
+    when 20..29
+      @user.level = "LVL 3 MASTER"
+      @user.save
+      "LVL 3 MASTER"
+    when 30..39
+      @user.level = "LVL 4 DIVINE"
+      @user.save
+      "LVL 4 DIVINE"
+    else
+      @user.level = "LVL 5 SUPREME"
+      @user.save
+      "LVL 5 SUPREME"
+    end
+  end
+
 end
