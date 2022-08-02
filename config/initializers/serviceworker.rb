24758ee84dd3b352b96b2f645e0792d3b6056404
bethany krasey <bethanykrasey@gmail.com>
2022-08-02 20:15:40 +1000

    attempting service worker

--- config/initializers/serviceworker.rb
+++ config/initializers/serviceworker.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   config.serviceworker.routes.draw do
     # map to assets implicitly
     match "/serviceworker.js"
-    match "/manifest.json"
+    match "/manifest.json.erb"
 
     # Examples
     #
